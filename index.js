b586c22104ba82d2d94142f75b08eab539cc6504
Martin Smidl <smidl.martin1@gmail.com>
2021-11-30 12:41:16 +0100

    add env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 const cities = require('./assets/city.list.json')
 
